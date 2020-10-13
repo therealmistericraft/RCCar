740b4ae9684b635febaeb3b07d27e46e3ca7b77f
therealmistericraft <65766422+therealmistericraft@users.noreply.github.com>
2020-10-13 16:54:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pygame
+import RPi.GPIO as GPIO
+import time
+
+
 
 events = pygame.event.get()
+for event in events:
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_UP:
+                        
