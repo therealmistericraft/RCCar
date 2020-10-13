8f176f9dead96415bfafd6409f668a0b7e98eb39
therealmistericraft <65766422+therealmistericraft@users.noreply.github.com>
2020-10-13 17:03:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ import pygame
 import RPi.GPIO as GPIO
 import time
 
+# Forewards: GPIO 7
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(7, GPIO.OUT)
+
+def on(pin):
+    GPIO.output(pin, GPIO.HIGH)
+def off(pin):
+    GPIO.output(pin, GPIO.LOW)
 
 events = pygame.event.get()
 for event in events:
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_UP:
-                        
-# hallo funktioniert das?
+            on(7)
